69c6a1a404a35dc6a98545a112904dc4ef8d34f2
Eric Schrijver <eric@ericschrijver.nl>
2013-05-21 13:26:15 +0200

    move e-mail munging outside of html body

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+/** 
+* Munged e-maillink that says sign up for newsletter 
+* @return {String}         Returns generated html as a txt string.
+*/
+function WPXUjgNncVJoKOS()
+{
+    var txt = ""
+    var uhxjELVpakNYRvT=["x65","x72","x69","x63","x40","101","x72","105","x63","x73","99","104","114","x69","106","x76","101","114","46","110","108"];
+    var OlIZTifXFqdvTow=['?','s','u','b','j','e','c','t','=','s','i','g','n',' ','u','p',' ','f','o','r',' ','n','e','w','s','l','e','t','t','e','r'];
+    var AsidCMSLcNXyxJh=["x73","105","103","x6e","x20","117","x70","x20","102","x6f","x72","x20","x74","x68","x65","x20","110","x65","119","x73","x6c","x65","x74","x74","101","114"];
+    txt += "<a href=\"&#x6d;&#97;&#x69;&#x6c;&#x74;&#000111;&#x3a;";
+    for (i=0; i<uhxjELVpakNYRvT.length; i++) txt += '&#'+uhxjELVpakNYRvT[i]+';';
+    for (i=0; i<OlIZTifXFqdvTow.length; i++) txt += OlIZTifXFqdvTow[i];
+    txt += '" style="" class="" id="">';
+    for (i=0; i<AsidCMSLcNXyxJh.length; i++) txt += '&#'+AsidCMSLcNXyxJh[i]+';';
+    txt += '</a>';
+    return txt;
+}
