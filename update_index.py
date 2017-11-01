4b5be64db4d088a3946e728f7ca673df34961ca0
Eric Schrijver <eric@ericschrijver.nl>
2017-11-01 13:25:57 +0100

    More readable code and comments for update_index.py

--- update_index.py
+++ update_index.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Update index.html by taking input from two sources:
+http://schr.fr/
+http://i.liketightpants.net/and/
+"""
+
 from time import strftime
 from lxml import etree, html
 from lxml.cssselect import CSSSelector
@@ -8,66 +14,59 @@ import re
 import codecs
 import feedparser
 
+# Instead of using a template, we read in and update the existing file
 htmltree = html.parse('index.html')
 
-# Syndication for the design portfolio
-a = feedparser.parse('http://schr.fr/feed')
+# Pull in syndication for the design portfolio
+design_feed = feedparser.parse('http://schr.fr/feed')
 
-# Syndication for the blog
-b = feedparser.parse('http://i.liketightpants.net/and/feed/us/recent_entries.xml')
+# Pull in syndication for the blog
+pants_feed = feedparser.parse('http://i.liketightpants.net/and/feed/us/recent_entries.xml')
 
+# On the index page, find the list html element for design projects
+# and clear the old values
 sel = CSSSelector(".pagina2 dl")
 design_dl = sel(htmltree)[0]
 design_dl.clear()
 
-n = 0
-for i in a['items']:
-    if n == 5:
-        break
-    year = strftime( '%Y', i['updated_parsed'])
-    dt = html.fragment_fromstring('<dt><a href="%s">%s</a> %s</dt>' % (i['link'], i['title'], year))
+# create the new html elements for design_dl and append them
+for post in design_feed['items'][:5]:
+    year = strftime( '%Y', post['updated_parsed'])
+    dt = html.fragment_fromstring('<dt><a href="%s">%s</a> %s</dt>' % (post['link'], post['title'], year))
     try:
-        img = html.fragment_fromstring(i.content[0].value, "div").cssselect('img')[0]
+        # find the first image in the post’s content
+        img = html.fragment_fromstring(post.content[0].value, "div").cssselect('img')[0]
+        # it’s probably re-sized by WP to 620xsomething,
+        # change the img to the 150x150 thumbnail version
         img.attrib['src'] = re.sub("[0-9]+x[0-9]+", "150x150", img.attrib['src'])
         img.attrib['height'] = '150'
         img.attrib['width'] = '150'
-        n += 1
     except:
         continue
     design_dl.append(dt)
     dd = html.Element('dd')
     link = html.Element('a')
-    link.attrib['href'] = i['link']
+    link.attrib['href'] = post['link']
     link.append(img)
     dd.append(link)
     design_dl.append(dd)
-    
-# print etree.tostring(design_dl, encoding='UTF-8', pretty_print=True)
 
 sel = CSSSelector(".pagina3 dl")
 pants_dl = sel(htmltree)[0]
 pants_dl.clear()
 
-n = 0
-for i in b['items']:
-    if n == 12:
-        break
-    dt = html.fragment_fromstring('<dt><a href="%s">%s</a></dt>' % (i['link'], i['title']))
-    updated_formatted = strftime( '%B %d, %Y', i['updated_parsed'])
+# create the new html elements for pants_dl and append them
+for post in pants_feed['items'][:12]:
+    dt = html.fragment_fromstring('<dt><a href="%s">%s</a></dt>' % (post['link'], post['title']))
+    updated_formatted = strftime( '%B %d, %Y', post['updated_parsed'])
     dd = html.fragment_fromstring( '<dd>%s</dd>' % (updated_formatted) )
     pants_dl.append(dt)
     pants_dl.append(dd)
-    n += 1
-    
+
+# to debug
+# print etree.tostring(design_dl, encoding='UTF-8', pretty_print=True)
 # print etree.tostring(pants_dl, encoding='UTF-8', pretty_print=True)
-
-
-
-
-#print """<dt>%s</dt>\n<dd><a href="%s">%s</a></dd>""" % (i['link'], i['title'], img)
-    #"<dt>%s</dt>\n" % i['title']
-    #print html.fragment_fromstring( """<dd><a href="%s">%s</a></dd>""" % (i['link'], img) )
 
 with open('index.html', 'wb') as f:
     f.write(etree.tostring(htmltree, encoding='UTF-8', pretty_print=True))
-# print etree.tostring(htmltree, encoding='UTF-8', pretty_print=True)
+    # print etree.tostring(htmltree, encoding='UTF-8', pretty_print=True)
